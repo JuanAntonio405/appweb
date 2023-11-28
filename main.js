8a3c2f6febc8597e7f19b9be02b0ad2b7c69bb59
appweb <anto83405@gmail.com>
2023-11-27 21:18:36 -0600

    1
    1

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
-//service worker
+//Service worker
 
 if('serviceWorker' in navigator){
-console.log('Puedes usar los serviceworker del navegador');
+    console.log('Puedes usar los serviceWorker del navegador');
 
-navigator.serviceworker.register('./sw.js')
-.then(res => console.log('serviceworker cargadp cprrectamente', res))
-.catch(err => console.log('serviceworker no se a podido registrar', err))
+    navigator.serviceWorker.register('./sw.js')
+                           .then(res => console.log('serviceWorker cargado correctamente', res))
+                           .catch(err => close.log('serviceWorker no se ha podido registrar', err))
 }else{
-console.log ('NO puedes usar los serviceworker del navegador');
+    console.log('NO Puedes usar los ServiceWorker del navegador');
 }
 
-//scroll suavisando
-$ (document).ready(function(){
 
-$("#menu a").click(function(){
-e-preventDefault();
-
-$(html, body). animate ({
-scrollTop: $($(this).attr("href"))-offset().top
-});
-return false;
-});
-
-});
+//scroll suavizado
+$(document).ready(function(){
+    
+    $("#menu a").click(function(e){
+        e.preventDefault();
+        
+        $("html, body").animate({
+            scrollTop: $($(this).attr('href')).offset().top
+        });
+        return false;
+    });
+})
